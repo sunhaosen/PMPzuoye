c2396176162521310aa6efdc74a68e70a727d38a
sunhaosen <949993468@qq.com>
2019-03-14 19:07:25 +0800

    11

--- 0314/3.php
+++ 0314/3.php
@@ -3,15 +3,15 @@ echo fbnq(10);
 function fbnq($n)
 {
     $arr = [];
-    for ($i = 1;$i <= $n; $i++ )
-    {
-        if ($i <= 2)
+    for ($i=0; $i <= $n; $i++) 
+    { 
+        if (count($arr) < 2) 
         {
             $arr[] = 1;
         }
         else
         {
-            $arr[] = ($i - 1) + ($i - 2);
+            $arr[] = $arr[$i - 2] + $arr[$i - 1];
         }
     }
     print_r($arr);
